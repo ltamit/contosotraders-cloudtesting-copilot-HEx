80b0af9acc3f0364dc14d09a595a33cdd988816e
lambdapro <mudassars@lambdatest.com>
2023-06-12 19:19:12 +0530

    removed script

--- src/ContosoTraders.Ui.Website/tests/verifymsg.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/verifymsg.spec.ts
@@ -4,12 +4,3 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/');
 });
 
-test.describe('msg validation', () => {
-  test('should validate msg', async ({ page }) => {
-    await page.fill('#msg', 'test');
-    await page.click('text=Send');
-    await page.waitForSelector('#msg-error');
-    const msgError = await page.$eval('#msg-error', (el) => el.textContent);
-    expect(msgError).toBe('msg is required');
-  });
-});
