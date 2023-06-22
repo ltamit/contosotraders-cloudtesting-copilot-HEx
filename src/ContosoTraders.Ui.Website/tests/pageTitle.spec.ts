8a68ad5a2b01f83538ba1c720b0a6191f42be832
lambdapro <mudassars@lambdatest.com>
2023-06-22 16:18:23 +0530

    updated tests

--- src/ContosoTraders.Ui.Website/tests/pageTitle.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/pageTitle.spec.ts
@@ -4,7 +4,10 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/');
 });
 
-test('should have the correct page title', async ({ page }) => {
-  const pageTitle = await page.title();
-  expect(pageTitle).toBe('Contoso Traders');
+test.describe('Header Message Validation', () => {
+  test('verify Title of Page', async ({ page }) => {
+    await page.getByTitle('Contoso Traders')
+    await expect(page).toHaveTitle('Contoso Traders');
+  })
+
 });
